1060e47508bef06c2bfd9e3b1d635f37171393b6
wong_chun_fung <t109aem001@ntut.org.tw>
2021-05-24 16:20:53 +0800

    Initial Commit

---
+++ lab09/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lab09
+//
+//  Created by Fung on 2021/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lab09/lab09App.swift
@@ -0,0 +1,17 @@
+//
+//  lab09App.swift
+//  lab09
+//
+//  Created by Fung on 2021/5/24.
+//
+
+import SwiftUI
+
+@main
+struct lab09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
